6d2fde357a242ec2e26d11ccfc10a788e7d3cdf1
YuyaTorii <Yuya_Torii@hulft.com>
2022-11-07 23:28:50 +0900

    first commit

---
+++ src/native-test/java/jp/co/saison/sis2/tvc/book/share/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package jp.co.saison.sis2.tvc.book.share;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
